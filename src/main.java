b957648640fe25c2aa21cd0e25ff8f751911af81
Omar Elfiki <o.elfiki@student.maastrichtuniversity.nl>
2025-03-10 17:56:50 +0100

    adding javafx demo

--- src/main.java
+++ src/main.java
@@ -1,5 +1,28 @@
-public class main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Demo");
+
+        Button btn = new Button();
+        btn.setText("Click me");
+        btn.setOnAction(event -> System.out.println("Button clicked!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        launch(args);
     }
 }
