3bf4171f96974f25486c50d3cc438c8b8af570c5
Omar Elfiki <o.elfiki@student.maastrichtuniversity.nl>
2025-04-17 22:16:16 +0200

    refactoring settings menu

--- src/homeUI.java
+++ src/homeUI.java
@@ -20,6 +20,7 @@ import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.shape.Circle;
+
 import java.util.Objects;
 
 public class homeUI extends Application {
@@ -72,11 +73,11 @@ public class homeUI extends Application {
         leftPane.getChildren().add(title);
 
         StackPane startGroup = createTextFieldWithIcon("↗", "Starting Point");
-        bindPosition(startGroup, root, 0.017, 0.09, 0.24, 0.035);
+        bindPosition(startGroup, root, 0.09);
         leftPane.getChildren().add(startGroup);
 
         StackPane endGroup = createTextFieldWithIcon("↙", "Destination");
-        bindPosition(endGroup, root, 0.017, 0.18, 0.24, 0.035);
+        bindPosition(endGroup, root, 0.18);
         leftPane.getChildren().add(endGroup);
 
         Text label = new Text("Navigate to see public transport \n options");
@@ -108,7 +109,7 @@ public class homeUI extends Application {
         hideSidePanel.layoutXProperty().bind(root.widthProperty().multiply(0.245));
         hideSidePanel.layoutYProperty().bind(root.heightProperty().multiply(0.46));
         hideSidePanel.setStyle("-fx-background-color: grey; -fx-text-fill: white;");
-        hideSidePanel.setOnAction(event -> {
+        hideSidePanel.setOnAction(_ -> {
             toggleLeftBar(leftPane);
             hideSidePanel.setVisible(false);
             showSidePanel.setVisible(true);
@@ -123,7 +124,7 @@ public class homeUI extends Application {
         StackPane.setAlignment(showSidePanel, Pos.CENTER_LEFT);
         showSidePanel.translateXProperty().bind(root.widthProperty().multiply(0.01));
         showSidePanel.translateYProperty().bind(root.heightProperty().multiply(-0.04));
-        showSidePanel.setOnAction(event -> {
+        showSidePanel.setOnAction(_ -> {
             toggleLeftBar(leftPane);
             showSidePanel.setVisible(false);
             hideSidePanel.setVisible(true);
@@ -145,7 +146,7 @@ public class homeUI extends Application {
         togglePane.layoutYProperty().bind(root.heightProperty().multiply(0.913).add(20)); // 760/832 + 20
 
         // Handle click event
-        togglePane.setOnMouseClicked(_ -> toggleSwitch(knob, background));
+        togglePane.setOnMouseClicked(_ -> toggleSwitch(background));
         leftPane.getChildren().add(togglePane);
 
         Text toggleText = new Text("Heatmap Mode");
@@ -169,8 +170,7 @@ public class homeUI extends Application {
         settings.setStyle("-fx-background-color: grey; -fx-text-fill: white;");
         settings.setOnAction(_ -> {
             toggleLeftBar(leftPane);
-            Pane settingsPane = setSettingMenu(root, leftPane);
-            vbox_left.getChildren().add(settingsPane);
+            showSettingsMenu(root, leftPane, vbox_left);
         });
         leftPane.getChildren().add(settings);
 
@@ -196,7 +196,7 @@ public class homeUI extends Application {
                     System.out.println("Connecting to Azure SQL");
                     gtfs = new DBaccess("rome-gtfs.database.windows.net", "rome-gtfs", "gtfsaccess", "Gtfs-142025");
                     gtfs.connect();
-                } else if (Integer.parseInt(type) == 1){
+                } else if (Integer.parseInt(type) == 1) {
                     System.out.println("Connecting to MySQL");
                     gtfs = new DBaccess(System.getProperty("DB_HOST"), System.getProperty("DB_PORT"), System.getProperty("DB_NAME"), System.getProperty("DB_USER"), System.getProperty("DB_PASSWORD"));
                     gtfs.connect();
@@ -212,7 +212,7 @@ public class homeUI extends Application {
         });
     }
 
-    private void toggleSwitch(Circle knob, Rectangle background) {
+    private void toggleSwitch(Rectangle background) {
         if (isOn.get()) {
             background.setFill(Color.LIGHTGRAY); // Off state
         } else {
@@ -254,230 +254,22 @@ public class homeUI extends Application {
         leftPane.setManaged(!isVisible);
     }
 
-    private void bindPosition(Region node, Pane root, double xRatio, double yRatio, double wRatio, double hRatio) {
-        node.layoutXProperty().bind(root.widthProperty().multiply(xRatio));
+    private void bindPosition(Region node, Pane root, double yRatio) {
+        node.layoutXProperty().bind(root.widthProperty().multiply(0.017));
         node.layoutYProperty().bind(root.heightProperty().multiply(yRatio));
-        node.prefWidthProperty().bind(root.widthProperty().multiply(wRatio));
-        node.prefHeightProperty().bind(root.heightProperty().multiply(hRatio));
+        node.prefWidthProperty().bind(root.widthProperty().multiply(0.24));
+        node.prefHeightProperty().bind(root.heightProperty().multiply(0.035));
     }
 
-    private Pane setSettingMenu(BorderPane root, Pane leftPane) {
-        Pane settingMenu = new Pane();
-
-        Rectangle settings = new Rectangle();
-        settings.widthProperty().bind(root.widthProperty().multiply(0.273)); // 350/1280
-        settings.heightProperty().bind(root.heightProperty()); // Full height
-        settings.setX(0);
-        settings.setY(0);
-        settings.setFill(Color.web("#5D5D5D"));
-        settings.setOpacity(0.95);
-        settingMenu.getChildren().add(settings);
-
-        Text title = new Text("Settings");
-        title.setFill(Color.WHITE);
-        title.setStyle("-fx-font: 28 Ubuntu;");
-        title.xProperty().bind(root.widthProperty().multiply(0.015)); // 20/1280
-        title.yProperty().bind(root.heightProperty().multiply(0.06)); // 50/832
-        settingMenu.getChildren().add(title);
-
-        Text label = new Text("GTFS .zip Path");
-        label.setTextAlignment(TextAlignment.CENTER);
-        label.setFill(Color.WHITE);
-        label.setStyle("-fx-font: 15 Ubuntu;");
-        label.xProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        label.yProperty().bind(root.heightProperty().multiply(0.12)); // 100/832
-        settingMenu.getChildren().add(label);
-
-        TextField textField = new TextField();
-        textField.setPromptText("Path to GTFS zip file");
-        textField.layoutXProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        textField.layoutYProperty().bind(root.heightProperty().multiply(0.144)); // 120/832
-        textField.prefWidthProperty().bind(root.widthProperty().multiply(0.24)); // 300/1280
-        textField.prefHeightProperty().bind(root.heightProperty().multiply(0.035)); // 30/832
-        settingMenu.getChildren().add(textField);
-        textField.setText(System.getProperty("GTFS_DIR"));
-
-        Text label2 = new Text("MySQL Connection Details");
-        label2.setTextAlignment(TextAlignment.CENTER);
-        label2.setFill(Color.WHITE);
-        label2.setStyle("-fx-font: 20 Ubuntu;");
-        label2.xProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        label2.yProperty().bind(root.heightProperty().multiply(0.23)); // 200/832
-        settingMenu.getChildren().add(label2);
-
-        Text host = new Text("Host");
-        host.setTextAlignment(TextAlignment.CENTER);
-        host.setFill(Color.WHITE);
-        host.setStyle("-fx-font: 15 Ubuntu;");
-        host.xProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        host.yProperty().bind(root.heightProperty().multiply(0.276)); // 230/832
-        settingMenu.getChildren().add(host);
-
-        TextField hostField = new TextField();
-        hostField.setPromptText("Host");
-        hostField.layoutXProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        hostField.layoutYProperty().bind(root.heightProperty().multiply(0.3)); // 250/832
-        hostField.prefWidthProperty().bind(root.widthProperty().multiply(0.24)); // 300/1280
-        hostField.prefHeightProperty().bind(root.heightProperty().multiply(0.035)); // 30/832
-        settingMenu.getChildren().add(hostField);
-        hostField.setText(System.getProperty("DB_HOST"));
-
-        Text user = new Text("User");
-        user.setTextAlignment(TextAlignment.CENTER);
-        user.setFill(Color.WHITE);
-        user.setStyle("-fx-font: 15 Ubuntu;");
-        user.xProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        user.yProperty().bind(root.heightProperty().multiply(0.385)); // 320/832
-        settingMenu.getChildren().add(user);
-
-        TextField userField = new TextField();
-        userField.setPromptText("User");
-        userField.layoutXProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        userField.layoutYProperty().bind(root.heightProperty().multiply(0.41)); // 340/832
-        userField.prefWidthProperty().bind(root.widthProperty().multiply(0.24)); // 300/1280
-        userField.prefHeightProperty().bind(root.heightProperty().multiply(0.035)); // 30/832
-        settingMenu.getChildren().add(userField);
-        userField.setText(System.getProperty("DB_USER"));
-
-        Text password = new Text("Password");
-        password.setTextAlignment(TextAlignment.CENTER);
-        password.setFill(Color.WHITE);
-        password.setStyle("-fx-font: 15 Ubuntu;");
-        password.xProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        password.yProperty().bind(root.heightProperty().multiply(0.48)); // 400/832
-        settingMenu.getChildren().add(password);
-
-        TextField passwordField = new TextField();
-        passwordField.setPromptText("Password");
-        passwordField.layoutXProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        passwordField.layoutYProperty().bind(root.heightProperty().multiply(0.5)); // 420/832
-        passwordField.prefWidthProperty().bind(root.widthProperty().multiply(0.24)); // 300/1280
-        passwordField.prefHeightProperty().bind(root.heightProperty().multiply(0.035)); // 30/832
-        settingMenu.getChildren().add(passwordField);
-        passwordField.setText(System.getProperty("DB_PASSWORD"));
-
-        Text port = new Text("Port");
-        port.setTextAlignment(TextAlignment.CENTER);
-        port.setFill(Color.WHITE);
-        port.setStyle("-fx-font: 15 Ubuntu;");
-        port.xProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        port.yProperty().bind(root.heightProperty().multiply(0.6)); // 500/832
-        settingMenu.getChildren().add(port);
-
-        TextField portField = new TextField();
-        portField.setPromptText("Port");
-        portField.layoutXProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        portField.layoutYProperty().bind(root.heightProperty().multiply(0.625)); // 520/832
-        portField.prefWidthProperty().bind(root.widthProperty().multiply(0.24)); // 300/1280
-        portField.prefHeightProperty().bind(root.heightProperty().multiply(0.035)); // 30/832
-        settingMenu.getChildren().add(portField);
-        portField.setText(System.getProperty("DB_PORT"));
-
-        Text database = new Text("Database");
-        database.setTextAlignment(TextAlignment.CENTER);
-        database.setFill(Color.WHITE);
-        database.setStyle("-fx-font: 15 Ubuntu;");
-        database.xProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        database.yProperty().bind(root.heightProperty().multiply(0.72)); // 600/832
-        settingMenu.getChildren().add(database);
-
-        TextField databaseField = new TextField();
-        databaseField.setPromptText("Database");
-        databaseField.layoutXProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        databaseField.layoutYProperty().bind(root.heightProperty().multiply(0.745)); // 620/832
-        databaseField.prefWidthProperty().bind(root.widthProperty().multiply(0.24)); // 300/1280
-        databaseField.prefHeightProperty().bind(root.heightProperty().multiply(0.035)); // 30/832
-        settingMenu.getChildren().add(databaseField);
-        databaseField.setText(System.getProperty("DB_NAME"));
-
-        Label testLabel = new Label();
-        testLabel.setText("");
-        testLabel.setTextFill(Color.WHITE);
-        testLabel.setStyle("-fx-font: 15 Ubuntu;");
-        testLabel.layoutXProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        testLabel.layoutYProperty().bind(root.heightProperty().multiply(0.95)); // 700/832
-        testLabel.prefWidthProperty().bind(root.widthProperty().multiply(0.24)); // 300/1280
-        testLabel.prefHeightProperty().bind(root.heightProperty().multiply(0.035)); // 30/832
-        settingMenu.getChildren().add(testLabel);
-
-        Button test = new Button("Test and Save");
-        test.setStyle("-fx-background-color: grey; -fx-text-fill: white;");
-        test.layoutXProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        test.layoutYProperty().bind(root.heightProperty().multiply(0.8)); // 700/832
-        test.prefWidthProperty().bind(root.widthProperty().multiply(0.24)); // 300/1280
-        test.prefHeightProperty().bind(root.heightProperty().multiply(0.035)); // 30/832
-        test.setOnAction(_ -> {
-            testLabel.setText("");
-            String hostText = hostField.getText();
-            String userText = userField.getText();
-            String passwordText = passwordField.getText();
-            String portText = portField.getText();
-            String databaseText = databaseField.getText();
-            DBaccess gtfs = new DBaccess(hostText, portText, databaseText, userText, passwordText);
-            System.setProperty("DB_PASSWORD", passwordText);
-            System.setProperty("DB_USER", userText);
-            System.setProperty("DB_HOST", hostText);
-            System.setProperty("DB_PORT", portText);
-            System.setProperty("DB_NAME", databaseText);
-            System.setProperty("GTFS_DIR", textField.getText());
-            ConfigLoader.saveConfig("config.properties");
-            gtfs.connect();
-            if (gtfs.conn != null) {
-                testLabel.setText("Connection Established");
-
-            } else {
-                testLabel.setText("Connection Failed");
-            }
-        });
-        settingMenu.getChildren().add(test);
-
-        Button importButton = new Button("Import GTFS");
-        importButton.setStyle("-fx-background-color: grey; -fx-text-fill: white;");
-        importButton.layoutXProperty().bind(root.widthProperty().multiply(0.017)); // 78/1280
-        importButton.layoutYProperty().bind(root.heightProperty().multiply(0.85)); // 700/832
-        importButton.prefWidthProperty().bind(root.widthProperty().multiply(0.24)); // 300/1280
-        importButton.prefHeightProperty().bind(root.heightProperty().multiply(0.035)); // 30/832
-//        importButton.setOnAction(event -> {
-//            String pathText = textField.getText();
-//            String hostText = hostField.getText();
-//            String userText = userField.getText();
-//            String passwordText = passwordField.getText();
-//            String portText = portField.getText();
-//            String databaseText = databaseField.getText();
-//
-//            GTFSaccess gtfs = new GTFSaccess(hostText, databaseText, userText, passwordText);
-//            gtfs.connect();
-//            gtfs.importGTFS(pathText);
-//        });
-        settingMenu.getChildren().add(importButton);
-
-        Line line = new Line();
-        line.startXProperty().bind(root.widthProperty().multiply(0)); // 0/1280
-        line.startYProperty().bind(root.heightProperty().multiply(0.89).add(20)); // 740/832 + 20
-        line.endXProperty().bind(root.widthProperty().multiply(0.273)); // 350/1280
-        line.endYProperty().bind(root.heightProperty().multiply(0.89).add(20)); // 740/832 + 20
-        line.setStroke(Color.WHITE);
-        settingMenu.getChildren().add(line);
-
-        Button close = new Button("X");
-        close.setStyle("-fx-background-color: grey; -fx-text-fill: white;");
-        close.layoutXProperty().bind(root.widthProperty().multiply(0.219)); // 280/1280
-        close.layoutYProperty().bind(root.heightProperty().multiply(0.94)); // 50/832
-        close.prefWidthProperty().bind(root.widthProperty().multiply(0.039)); // 50/1280
-        close.prefHeightProperty().bind(root.heightProperty().multiply(0.036)); // 30/832
-        close.setOnAction(_ -> {
-            settingMenu.setVisible(false);
-            settingMenu.setManaged(false);
-            toggleLeftBar(leftPane);
-        });
-        settingMenu.getChildren().add(close);
-
-        return settingMenu;
+    private void showSettingsMenu(BorderPane root, Pane leftPane, VBox vbox_left) {
+        settingsUI settingsUI = new settingsUI(root, leftPane);
+        Pane settingsPane = settingsUI.createSettingsMenu();
+        vbox_left.getChildren().add(settingsPane);
     }
 
 
     public static void main(String[] args) {
-        if (!ConfigLoader.checkIfConfigExists("config.properties")){
+        if (!ConfigLoader.checkIfConfigExists("config.properties")) {
             ConfigLoader.createConfig("config.properties");
         } else {
             System.out.println("Config file found");
