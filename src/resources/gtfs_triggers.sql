9d1a3e23ab8a09968ea93a51069ab26234d8d32a
Omar Elfiki <o.elfiki@student.maastrichtuniversity.nl>
2025-05-09 11:05:18 +0200

    new gtfs function

--- src/resources/gtfs_triggers.sql
+++ src/resources/gtfs_triggers.sql
@@ -1,20 +1,12 @@
-create function get_closest_stops(lat float, lon float, radius float) returns json
+create function get_closest_stops(lat float, lon float, radius float) returns resultset
     deterministic
 BEGIN
-    DECLARE result JSON;
-
-SELECT JSON_ARRAYAGG(
-               JSON_OBJECT(
-                       'stop_id', s.stop_id,
-                       'stop_name', s.stop_name,
-                       'stop_lat', s.stop_lat,
-                       'stop_lon', s.stop_lon,
-                       'distance', SQRT(POW(s.stop_lat - lat, 2) + POW(s.stop_lon - lon, 2))
-               )
-       )
-INTO result
-FROM stops s
-WHERE SQRT(POW(s.stop_lat - lat, 2) + POW(s.stop_lon - lon, 2)) <= radius;
-
-RETURN result;
+    RETURN SELECT
+               s.stop_id,
+               s.stop_name,
+               s.stop_lat,
+               s.stop_lon,
+               SQRT(POW(s.stop_lat - lat, 2) + POW(s.stop_lon - lon, 2)) AS distance
+           FROM stops s
+           WHERE SQRT(POW(s.stop_lat - lat, 2) + POW(s.stop_lon - lon, 2)) <= radius;
 END;
