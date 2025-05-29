7774b02c001dceb0848f5225d98b5e14d19de130
ItsNotFill <itsnotfill@gmail.com>
2025-05-29 14:17:52 +0200

    Createing the graph through a Dijkstra since we dont have a sigle goal( i just set the h=0 and didnt create any new classes for the nodes
    also created two versions, one with walking option and one without since they get different results and we can decide later what to keep
    
    FM
    if any more questions call me, no worries

--- src/main/java/util/EdgeService.java
+++ src/main/java/util/EdgeService.java
@@ -69,4 +69,44 @@ public class EdgeService {
 
         return edges;
     }
+    public ArrayList<Edge> getEdgesNoWalk(Node node) {
+        isDebugMode = getDebugMode();
+        ArrayList<Edge> edges = new ArrayList<>();
+        Stop startStop = tds.getStop(node.stopId);
+
+        // add the stop that can be reached directly by following the same route
+        if (node.trip != null ) {
+            try {
+                TripEdge tripEdge = new TripEdge(
+                        startStop.stopId,
+                        node.arrivalTime,
+                        node.trip
+                );
+                edges.add(tripEdge);
+            } catch (IllegalArgumentException e) {
+                if (isDebugMode) System.err.println("Skipping trip continuation: " + e.getMessage());
+            }
+
+        }
+
+
+        //add transfer edges
+        List<Trip> upcomingTrips = tds.getUpcomingDistinctRouteTrips(node.stopId, node.arrivalTime);
+        if (isDebugMode) System.err.println("upcoming trips: " + upcomingTrips.size());
+        for (Trip trip : upcomingTrips) {
+            try {
+                TransferEdge transferEdge = new TransferEdge(startStop.stopId, node.arrivalTime, trip);
+                edges.add(transferEdge);
+                if (isDebugMode) System.err.println("Transfer " + transferEdge.fromStopId + " to " + transferEdge.toStopId +
+                        " weight " + transferEdge.weight + " by route " + transferEdge.trip.getRoute().routeId +
+                        " at " + transferEdge.departureTime + " waiting until " + transferEdge.rideStartTime +
+                        " to " + transferEdge.arrivalTime);
+            } catch (IllegalArgumentException e) {
+                if (isDebugMode) System.err.println("Skipping trip " + trip.tripId + ": " + e.getMessage());
+            }
+        }
+
+
+        return edges;
+    }
 }
