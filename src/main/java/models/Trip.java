62237b20d8226ca999211298097ef4000470b495
Omar Elfiki <o.elfiki@student.maastrichtuniversity.nl>
2025-06-04 13:41:39 +0200

    routing fixes

--- src/main/java/models/Trip.java
+++ src/main/java/models/Trip.java
@@ -2,11 +2,17 @@ package models;
 
 public record Trip(String tripId, Route route, String headSign) {
 
+    public Trip {
+        if (route == null) {
+            route = new Route("unknown", null, "N/A", "Unknown");
+        }
+    }
+
     @Override
     public String toString() {
         return "Trip{" +
                 "tripId='" + tripId + '\'' +
-                ", route=" + (route != null ? route.routeId() : "null") +
+                ", route=" + (route != null ? route.routeId() : "N/A") +
                 '}';
     }
 }
