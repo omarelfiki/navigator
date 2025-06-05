0936492b5630f927c6cf38a0c9325412d8e4204e
Vlad Rusu <v.rusu@student.maastrichtuniversity.nl>
2025-06-05 15:45:54 +0200

    walk json fixed?

--- src/main/java/com/navigator14/RoutingEngine.java
+++ src/main/java/com/navigator14/RoutingEngine.java
@@ -160,16 +160,19 @@ public class RoutingEngine {
             );
 
             if (Objects.equals(current.getMode(), "WALK")) {
-                if (current.getParent() == null) {
-                    to = Map.of("lat", request.latStart(), "lon", request.lonStart());
-                }
+                double fromLat = current.getParent() == null ? request.latStart() : current.getParent().getStop().getStopLat();
+                double fromLon = current.getParent() == null ? request.lonStart() : current.getParent().getStop().getStopLon();
+
                 result.add(Map.of(
                         "mode", "walk",
                         "to", to,
                         "duration", durationMinutes,
-                        "startTime", formattedStartTime
+                        "startTime", formattedStartTime,
+                        "fromLat", fromLat,
+                        "fromLon", fromLon
                 ));
-            } else if (Objects.equals(current.getMode(), "SAME_TRIP") || Objects.equals(current.getMode(), "TRANSFER")) {
+            }
+            else if (Objects.equals(current.getMode(), "SAME_TRIP") || Objects.equals(current.getMode(), "TRANSFER")) {
                 result.add(Map.of(
                         "mode", "ride",
                         "to", to,
